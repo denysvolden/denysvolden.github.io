a0e15a20dcde3bb7c15461d7cc91b699539a757b
denysvolden.github.io <den-pochta@mail.ru>
2017-06-27 20:30:16 +0300

    qwer

--- js/common.js
+++ js/common.js
@@ -48,8 +48,8 @@ $(document).ready(function() {
 
 	//Попап менеджер FancyBox
 	//Документация: http://fancybox.net/howto
-	//<a class="fancybox"><img src="image.jpg" /></a>
-	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
+	//<a class="fancybox"><img src="image.png" /></a>
+	//<a class="fancybox" data-fancybox-group="group"><img src="image.png" /></a>
 	$(".fancybox").fancybox();
 
 	//Добавляет классы дочерним блокам .block для анимации
